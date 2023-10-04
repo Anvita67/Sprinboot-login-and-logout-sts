6ab8df89c8420e9a105c04366e562c0491eb5657
Anvita67 <118798794+Anvita67@users.noreply.github.com>
2023-10-04 11:21:37 +0530

    Add files via upload

---
+++ sb-Login-logout-mysql/src/main/java/com/app/SbLoginLogoutMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbLoginLogoutMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbLoginLogoutMysqlApplication.class, args);
+	}
+
+}
